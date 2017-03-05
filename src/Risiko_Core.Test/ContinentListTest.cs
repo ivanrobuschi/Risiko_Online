96acc81309489ac809f8df1b3b85bd36e5eb884a
rrobuschi <rrobuschi@gmail.com>
2017-03-05 18:04:48 +0100

    Added ContinentList class with tests

---
+++ src/Risiko_Core.Test/ContinentListTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using Risiko_Core.Classes;
+
+namespace Risiko_Core.Test
+{
+    class ContinentListTest
+    {
+        [Test]
+        public void ContinentListShouldExist()
+        {
+            var sut = new ContinentList();
+            sut.Should().NotBeNull();
+        }
+
+        [Test]
+        public void ContinentListShouldContainAllColors()
+        {
+            var sut = new ContinentList();
+            sut.Count.Should().Be(6);
+        }
+
+        [TestCase(0, "North America")]
+        [TestCase(1, "South America")]
+        [TestCase(2, "Africa")]
+        [TestCase(3, "Europe")]
+        [TestCase(4, "Asia")]
+        [TestCase(5, "Oceania")]
+        public void IdShouldMatchContinent(int id, string continentName)
+        {
+            var sut = new ContinentList();
+            sut.GetContinent(id).Should().Be(continentName);
+        }
+    }
+}
---
+++ src/Risiko_Core/Classes/ContinentList.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Risiko_Core.Classes
+{
+    public class ContinentList
+    {
+        private readonly List<string> _contintentList;
+
+        public ContinentList()
+        {
+            _contintentList = new List<string> { "North America", "South America", "Africa", "Europe", "Asia", "Oceania" };
+        }
+
+        public int Count
+        {
+            get { return _contintentList.Count; }
+        }
+
+        public string GetContinent(int id)
+        {
+            return _contintentList[id];
+        }
+    }
+}
