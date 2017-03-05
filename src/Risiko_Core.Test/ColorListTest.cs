b0b8319369d6b20d064d12d4e35d2f4e599c8dd6
Ivan Robuschi <robuschiivan@gmail.com>
2017-03-05 17:05:29 +0100

    Added ColorList class with tests

---
+++ src/Risiko_Core.Test/ColorListTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace Risiko_Core.Test
+{
+    public class ColorListTest
+    {
+        [Test]
+        public void ColorListShouldExist()
+        {
+            var sut = new ColorList();
+            sut.Should().NotBeNull();
+        }
+
+        [Test]
+        public void ColorListShouldContainAllColors()
+        {
+            var sut = new ColorList();
+            sut.colorList.Count.Should().Be(6);
+        }
+
+        [TestCase(0, "black")]
+        [TestCase(1, "green")]
+        [TestCase(2, "purple")]
+        [TestCase(3, "cyan")]
+        [TestCase(4, "yellow")]
+        [TestCase(5, "red")]
+        public void IDsShouldMatchColors(int id, string color)
+        {
+            var sut = new ColorList();
+            sut.colorList[id].Should().Be(color);
+        }
+    }
+}
---
+++ src/Risiko_Core/Classes/ColorList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Risiko_Core
+{
+
+
+    public class ColorList
+    {
+        public List<string> colorList;
+        public ColorList()
+        {
+            colorList = new List<string>();
+            colorList.Add("black");
+            colorList.Add("green");
+            colorList.Add("purple");
+            colorList.Add("cyan");
+            colorList.Add("yellow");
+            colorList.Add("red");
+        }
+
+    }
+}
