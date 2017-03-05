6fb958f6611a3273b77ee69a23440f97dc22b139
Ivan Robuschi <robuschiivan@gmail.com>
2017-03-05 18:21:39 +0100

    Added ID, name, colorID and targetID properties to player class with tests

--- src/Risiko_Core.Test/PlayerTest.cs
+++ src/Risiko_Core.Test/PlayerTest.cs
@@ -14,9 +14,38 @@ namespace Risiko_Core.Test
         [Test]
         public void PlayerShouldExist()
         {
-            var sut = new Player();
+            var sut = new Player(0, "pippo", 0, 0);
             sut.Should().NotBeNull();
         }
+
+        [Test]
+        public void PlayerIDShouldMatchGivenID()
+        {
+            var sut = new Player(0, "pippo", 1, 1);
+            sut.ID.Should().Be(0);
+        }
+
+        [Test]
+        public void PlayerNameShouldMatchGivenName()
+        {
+            var sut = new Player(0, "pippo", 0, 0);
+            sut.name.Should().Be("pippo");
+        }
+
+        [Test]
+        public void PlayerColorIDShouldMatchGivenColorID()
+        {
+            var sut = new Player(1, "pippo", 0, 1);
+            sut.colorID.Should().Be(0);
+        }
+
+        [Test]
+        public void PlayerTargetIDShouldMatchGivenTargetID()
+        {
+            var sut = new Player(1, "pippo", 1, 0);
+            sut.targetID.Should().Be(0);
+        }
+
 
     }
 }
--- src/Risiko_Core/Player.cs
+++ src/Risiko_Core/Player.cs
@@ -8,5 +8,17 @@ namespace Risiko_Core
 {
     public class Player
     {
+        public readonly int ID;
+        public readonly string name;
+        public readonly int colorID;
+        public readonly int targetID;
+
+        public Player(int _ID, string _name, int _colorID, int _targetID)
+        {
+            ID = _ID;
+            name = _name;
+            colorID = _colorID;
+            targetID = _targetID;
+        }
     }
 }
