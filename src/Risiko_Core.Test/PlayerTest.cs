d21a8d11eff292298eed20f440951f32afdc6d22
Ivan Robuschi <robuschiivan@gmail.com>
2017-03-05 17:40:45 +0100

    Added Player class with basic test

---
+++ src/Risiko_Core.Test/PlayerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentAssertions;
+using Risiko_Core;
+
+namespace Risiko_Core.Test
+{
+    class PlayerTest
+    {
+        [Test]
+        public void PlayerShouldExist()
+        {
+            var sut = new Player();
+            sut.Should().NotBeNull();
+        }
+
+    }
+}
